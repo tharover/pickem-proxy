9289e461a39f049bad4cccc623e4aa398d17593a
tharover <tharover@gmail.com>
2025-07-12 19:16:38 -0400

    adding OPTIONS handler back in

--- api/proxy.js
+++ api/proxy.js
@@ -3,6 +3,13 @@ export default async function handler(req, res) {
     const appScriptUrl = 'https://script.google.com/macros/s/AKfycbwPMF0tBSsCmm19z3pEN2yiXc1oXINfsu1-a-JYJ8-L9qn4w_0RDrNzWW7fNCZaelH-/exec';
     const appScriptTestUrl = 'https://script.google.com/macros/s/AKfycbyVdx3sircXk2_N4qSWoDg-jMWvkL1ub0bMjTb__jM/dev';
 
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        return res.status(200).end();
+    }
+    
     try {
         const response = await fetch(appScriptUrl, {
             method,
