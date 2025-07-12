9bea39f8ca9b9ec3262f3c8b44949f9a0687e48f
tharover <tharover@gmail.com>
2025-07-12 18:42:21 -0400

    Changing to Vercel serverless

--- api/proxy.js
+++ api/proxy.js
@@ -1,15 +1,14 @@
-const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const appScriptUrl = 'https://script.google.com/macros/s/AKfycbwPMF0tBSsCmm19z3pEN2yiXc1oXINfsu1-a-JYJ8-L9qn4w_0RDrNzWW7fNCZaelH-/exec';
 const appScriptTestUrl = 'https://script.google.com/macros/s/AKfycbyVdx3sircXk2_N4qSWoDg-jMWvkL1ub0bMjTb__jM/dev';
 
-const app = express();
-
-app.use('/api', createProxyMiddleware({
-  target: appScriptTestUrl,
+const proxy = createProxyMiddleware({
+  target: appScriptUrl,
   changeOrigin: true,
   pathRewrite: { '^/api': '' },
-}));
+});
 
-module.exports = app;
+module.exports = (req, res) => {
+  return proxy(req, res);
+};
